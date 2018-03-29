bfd00e8368cba776db49a549baf942dac91841d0
UMaster <luciferlikeme@163.com>
2018-03-29 11:47:54 +0800

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import {Grid, Row, Col} from 'react-bootstrap';
-import '../ '
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import ComponentA from './component/ComponentList/ComponentA'
 import ComponentB from './component/ComponentList/ComponentB'
+import DropDownMenuRight from './component/DropDownMenu/DropDownMenu'
 
-import Collapsebtn from './component/Collapse/Collapsebtn'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Grid>
-          <Row className="show-grid">
-          <Col xs={2} sm={2} lg={1} className="leftSide">
-          <Collapsebtn>
-            <Link to="/ComponentA">2222</Link>
-          </Collapsebtn>
-          <Collapsebtn>
-            <Link to="/ComponentB">3333</Link>
-          </Collapsebtn>
-          <Collapsebtn></Collapsebtn>
-          <Collapsebtn></Collapsebtn>
-          <div  className="bottom">11111</div>
-        </Col>
-        <Col xs={8} sm={8}>
-          <Route path="/ComponentA" component={ComponentA}></Route>
-          <Route path="/ComponentB" component={ComponentB}></Route>
-        </Col>
-        </Row>       
-        </Grid>
-      
-          
-      </BrowserRouter>
+      <MuiThemeProvider>
+      <DropDownMenuRight></DropDownMenuRight>
+      </MuiThemeProvider>
+        
     );
   }
 }

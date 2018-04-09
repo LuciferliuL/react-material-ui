caee102fbaf6d6767c01168e4feab047acf86e28
UMaster <luciferlikeme@163.com>
2018-04-09 10:19:16 +0800

    增加树结构

--- src/component/ComponentList/ComponentA.jsx
+++ src/component/ComponentList/ComponentA.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Layout} from 'element-react'
 
-export default class ComponentA extends React.Component{
-    render(){
-        return(
+import Tabs_L from '../Tabs/Tabs_L'
+
+export default class ComponentA extends React.Component {
+    render() {
+        return (
             <div>
-                123
+                <Layout.Row>
+                    <Layout.Col span="4">
+                       <Tabs_L></Tabs_L>
+                    </Layout.Col>
+                    <Layout.Col span="auto">2222</Layout.Col>
+                </Layout.Row>
             </div>
         )
     }
